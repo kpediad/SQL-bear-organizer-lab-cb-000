a617e5c00d9c4e799ea19d94ec355c377e9b2c8a
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-10-10 09:25:37 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES ('Mr. Chocolate', 10, 'Male', 'Brown', 'Moody', 1);
+INSERT INTO bears VALUES ('Rowdy', 10, 'Male', 'Brown', 'Moody', 1);
+INSERT INTO bears VALUES ('Tabitha', 10, 'Female', 'Brown', 'Moody', 1);
+INSERT INTO bears VALUES ('Sergeant Brown', 10, 'Male', 'Brown', 'Moody', 1);
+INSERT INTO bears VALUES ('Melissa', 10, 'Female', 'Brown', 'Moody', 1);
+INSERT INTO bears VALUES ('Grinch', 10, 'Male', 'Brown', 'Moody', 1);
+INSERT INTO bears VALUES ('Wendy', 10, 'Female', 'Brown', 'Moody', 1);
+INSERT INTO bears VALUES (NULL, 10, 'Female', 'Brown', 'Moody', 1);
